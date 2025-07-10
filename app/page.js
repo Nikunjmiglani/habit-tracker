697678ae88c6ae8fab19424b8a1fff216bd86b65
Nikunj Miglani <niikkunjmiglani@gmail.com>
2025-07-10 16:21:30 +0530

    vercel

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="border-t border-gray-800 mx-6 mt-3 sm:mx-20"></div>
     <section className=" text-black text-center mt-20">
       <h1 className="font-semibold font-mono  text-4xl">Track Habits. Build Streaks. Change Your Life.</h1>
-      <h2 className="mt-3 font-mono font-normal">Just Habits. No Noise.</h2>
+      <h2 className="mt-5 font-mono font-normal">Just Habits. No Noise.</h2>
 
      <Link href="/auth/signin">
           <button className="mt-10 px-7 cursor-pointer hover:scale-110 transition-transform duration-200 py-3 border text-white bg-black font-mono border-black rounded-full">
