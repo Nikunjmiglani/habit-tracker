315a101f0c415ff0e14bf1ebf1ee653015f2f811
Nikunj Miglani <niikkunjmiglani@gmail.com>
2025-07-10 17:08:15 +0530

    google

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import AuthProvider from '@/components/SessionProvider'; 
 import Navbar from '@/components/Navbar';
+import Head from 'next/head';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
 
@@ -12,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="sRnvIvrZfA2GB5-F0_x6WYLYNaIeQfztA5yh0QnB4Rs" />
+      </Head>
       <body>
         <ThemeProvider>
         <AuthProvider>
