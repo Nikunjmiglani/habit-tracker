3837e8af47956681dacb8eb1f74082368dcac9ef
Nikunj Miglani <niikkunjmiglani@gmail.com>
2025-07-10 17:31:38 +0530

    consent

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,9 +11,30 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "openid email profile",
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async signIn({ account, profile }) {
+      console.log("SIGN-IN ACCOUNT:", account);
+      console.log("SIGN-IN PROFILE:", profile);
+      return true;
+    },
+    async jwt({ token }) {
+      console.log("JWT CALLBACK:", token);
+      return token;
+    },
+    async session({ session, token }) {
+      console.log("SESSION CALLBACK:", session);
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
