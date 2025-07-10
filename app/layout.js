d310bedb3f1abfc4cfbf46400f2a8bec03772433
Nikunj Miglani <niikkunjmiglani@gmail.com>
2025-07-10 17:12:25 +0530

    domains

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,28 @@
 import './globals.css';
 import AuthProvider from '@/components/SessionProvider'; 
 import Navbar from '@/components/Navbar';
-import Head from 'next/head';
 import { ThemeProvider } from '@/components/ThemeProvider';
-
 
 export const metadata = {
   title: 'Habit Tracker',
   description: 'Track your daily habits',
+  other: {
+    'google-site-verification': 'sRnvIvrZfA2GB5-F0_x6WYLYNaIeQfztA5yh0QnB4Rs',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="google-site-verification" content="sRnvIvrZfA2GB5-F0_x6WYLYNaIeQfztA5yh0QnB4Rs" />
-      </Head>
       <body>
         <ThemeProvider>
-        <AuthProvider>
-          <Navbar/>
-          {children}
-        </AuthProvider>
+          <AuthProvider>
+            <Navbar/>
+            {children}
+          </AuthProvider>
         </ThemeProvider>
       </body>
     </html>
   );
 }
+
